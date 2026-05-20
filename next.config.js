177fccd5d72112db022bdead95815f38d6d04fd9
Shanov23 <107348646+Shanov23@users.noreply.github.com>
2026-05-21 01:35:23 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [{ source: '/api/:path*', headers: [{ key: 'Cache-Control', value: 'no-store' }] }]
-  }
-}
-module.exports = nextConfig
